f9dd5b2ed9b9529258bf5d184981c776b0f18cf7
inxendere <taief.ahmed04@gmail.com>
2024-01-20 18:46:01 -0500

    loading screen implemented

--- Assets/Scripts/ChatGPTManager.cs
+++ Assets/Scripts/ChatGPTManager.cs
@@ -6,15 +6,30 @@ using TMPro;
 
 public class ChatGPTManager : MonoBehaviour
 {
+    /// <summary>
+    /// Note: you have to put your own ChatGPT api in the user folder
+    /// In your user folder in C drive, create a folder and call it .openai
+    /// In that folder, create a json called auth.json
+    /// In there, fill in the following:
+    /// {
+    ///  "api_key": "YOUR API KEY HERE",
+    ///  "organization": "YOUR ORGANIZATION HERE"
+    /// }
+    /// </summary>
+
     private OpenAIApi openAI = new OpenAIApi();
     private List<ChatMessage> messages = new List<ChatMessage>();
-    // Start is called before the first frame update
-
     public TextMeshProUGUI responseText;
 
+    private void Awake()
+    {
+        responseText.text = "Waiting for your input...";
+    }
 
     public async void AskChatGPT(string newText)
     {
+        responseText.text = "Looking for ingredients...";
+
         ChatMessage newMessage = new ChatMessage();
         newMessage.Content = newText;
         newMessage.Role = "user";
@@ -26,18 +41,32 @@ public class ChatGPTManager : MonoBehaviour
         // request.Model = "text-embedding-ada-002";
         request.Model = "gpt-4-1106-preview";
 
+        StartCoroutine(StartLoadingAnimation());
+
         var response = await openAI.CreateChatCompletion(request);
 
         if (response.Choices != null && response.Choices.Count > 0)
         {
+            // StopCoroutine(StartLoadingAnimation());
+            StopAllCoroutines();
             var chatResponse = response.Choices[0].Message;
 
             messages.Add(chatResponse);
 
             Debug.Log(chatResponse.Content);
 
-            responseText.text = chatResponse.Content.ToString();
+            responseText.text = chatResponse.Content;
         }
+    }
 
+    IEnumerator StartLoadingAnimation()
+    {
+        responseText.text = "Looking for ingredients...";
+        yield return new WaitForSeconds(1f);
+        responseText.text = "Looking for ingredients....";
+        yield return new WaitForSeconds(1f);
+        responseText.text = "Looking for ingredients.....";
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(StartLoadingAnimation());
     }
 }
