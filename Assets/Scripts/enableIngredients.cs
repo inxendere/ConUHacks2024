b053ca952f507533c44a517b82dcae76df1cf962
inxendere <taief.ahmed04@gmail.com>
2024-01-21 08:02:51 -0500

    lookin good

--- Assets/Scripts/enableIngredients.cs
+++ Assets/Scripts/enableIngredients.cs
@@ -4,8 +4,16 @@ public class EnableGameObjectOnClick : MonoBehaviour
 {
     public GameObject objectToEnable; // Assign this in the inspecto
 
+    public Animator searchBoxAnim;
+
     public void enableObject()
     {
         objectToEnable.SetActive(true);
+
+
+        searchBoxAnim.SetBool("hasClicked", true);
+
     }
+
+
 }
